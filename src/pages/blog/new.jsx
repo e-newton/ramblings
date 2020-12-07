189bf677803a1019f59f6c1dd33ba1ad11719b16
e-newton <ericnewton2251@gmail.com>
2020-12-07 15:55:03 -0800

    🦞 Changed our new blog system into a function component.

--- src/pages/blog/new.jsx
+++ src/pages/blog/new.jsx
@@ -1,63 +1,49 @@
-import React, {Component} from 'react';
-import {Editor} from '@tinymce/tinymce-react'
+import React, {useState} from 'react';
 import App from "../../components/App";
+import {Editor} from "@tinymce/tinymce-react";
 import {firestore} from "../../lib/firebase";
-import {useAuth} from "../../hooks/useAuth";
 
-class NewBlog extends Component {
+function NewBlog() {
 
-    state = {
-        value: '',
-        content: '',
-    }
+    const [state, setState] = useState({url: '', content: ''})
 
-    constructor(props) {
-        super(props);
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.handleEditorChange = this.handleEditorChange.bind(this);
-        this.postBlog = this.postBlog.bind(this)
-        console.log(this.props)
-
-    }
-
-
-    handleEditorChange = (content, editor) => {
-        this.setState({
-            value: this.state.value,
-            content: content
-        })
-
-    }
-
-    handleTitleChange(event) {
-        let title = event.target.value.toString().toLowerCase().trim().replaceAll(' ', '_')
-        title = title.replaceAll(/[^a-z^A-Z^0-9_-]+/gi, "")
-        this.setState({
-            value: title,
-            content : this.state.content
-           })
-
-    }
-
-    async postBlog() {
+    const postBlog = async() => {
         let url = document.getElementById('urlinput').value
         let title = document.getElementById('blogTitleInput').value.toString().trim()
         console.log('what we are posting', {
-                body: this.state.content,
+                body: state.content,
                 date: new Date(),
                 title: title
             })
         await firestore.collection('blogs').doc(url).set({
-            body: this.state.content,
+            body: state.content,
             date: new Date(),
             title: title
         })
 
     }
 
+    const handleEditorChange = (content, editor) => {
+        console.log(content)
+        setState({
+            value: state.value,
+            content: content,
+            title: state.title
+        })
+    }
 
-    render() {
-        return (
+    const handleTitleChange = (event) => {
+        let title = event.target.value.toString().toLowerCase().trim().replaceAll(' ', '_')
+        title = title.replaceAll(/[^a-z^A-Z^0-9_-]+/gi, "")
+        setState({
+            value: title,
+            content : state.content,
+            title: state.title
+           })
+
+    }
+
+    return (
             <App>
                 <div className={"container-fluid"}>
                     <form>
@@ -66,14 +52,14 @@ class NewBlog extends Component {
                                 <div className="form-group">
                                     <label htmlFor="blogTitleInput">Blog Title</label>
                                     <input type="text" className="form-control" id="blogTitleInput"
-                                           aria-describedby="emailHelp" placeholder="Title" onChange={this.handleTitleChange}/>
+                                           aria-describedby="emailHelp" placeholder="Title" value={state.title} onChange={handleTitleChange}/>
                                 </div>
                             </div>
                             <div className={'col-6'}>
                                 <div className="form-group">
                                     <label htmlFor="urlinput">Blog URL</label>
                                     <input readOnly={true} type="text" className="form-control" id="urlinput"
-                                           aria-describedby="emailHelp" placeholder="Title" value={this.state.value}/>
+                                           aria-describedby="emailHelp" placeholder="Title" value={state.value}/>
                                 </div>
                             </div>
                         </div>
@@ -93,19 +79,123 @@ class NewBlog extends Component {
                                 alignleft aligncenter alignright alignjustify code image | \
                                 bullist numlist outdent indent | removeformat | fullscreen help'
                         }}
-                        onEditorChange = {this.handleEditorChange}
+                        onEditorChange = {handleEditorChange}
                     />
                     <div className={'row p-2 justify-content-center'}>
                         <div className={'col-4 d-flex justify-content-center'}>
-                            <button className={'btn btn-primary w-100'} onClick={this.postBlog}>Post Blog</button>
+                            <button className={'btn btn-primary w-100'} onClick={postBlog}>Post Blog</button>
                         </div>
                     </div>
                 </div>
 
             </App>
 
-        );
-    }
+    );
 }
 
+
+// class NewBlog extends Component {
+//
+//     // state = {
+//     //     value: '',
+//     //     content: '',
+//     // }
+//     //
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.handleTitleChange = this.handleTitleChange.bind(this);
+//     //     this.handleEditorChange = this.handleEditorChange.bind(this);
+//     //     this.postBlog = this.postBlog.bind(this)
+//     //     console.log(this.props)
+//     //
+//     // }
+//     //
+//     //
+//     // handleEditorChange = (content, editor) => {
+//     //     this.setState({
+//     //         value: this.state.value,
+//     //         content: content
+//     //     })
+//     //
+//     // }
+//     //
+//     // handleTitleChange(event) {
+//     //     let title = event.target.value.toString().toLowerCase().trim().replaceAll(' ', '_')
+//     //     title = title.replaceAll(/[^a-z^A-Z^0-9_-]+/gi, "")
+//     //     this.setState({
+//     //         value: title,
+//     //         content : this.state.content
+//     //        })
+//     //
+//     // }
+//     //
+//     // async postBlog() {
+//     //     let url = document.getElementById('urlinput').value
+//     //     let title = document.getElementById('blogTitleInput').value.toString().trim()
+//     //     console.log('what we are posting', {
+//     //             body: this.state.content,
+//     //             date: new Date(),
+//     //             title: title
+//     //         })
+//     //     await firestore.collection('blogs').doc(url).set({
+//     //         body: this.state.content,
+//     //         date: new Date(),
+//     //         title: title
+//     //     })
+//     //
+//     // }
+//
+//
+//     render() {
+//         return (
+//             <App>
+//                 <div className={"container-fluid"}>
+//                     <form>
+//                         <div className={'row'}>
+//                             <div className={'col-6'}>
+//                                 <div className="form-group">
+//                                     <label htmlFor="blogTitleInput">Blog Title</label>
+//                                     <input type="text" className="form-control" id="blogTitleInput"
+//                                            aria-describedby="emailHelp" placeholder="Title" onChange={this.handleTitleChange}/>
+//                                 </div>
+//                             </div>
+//                             <div className={'col-6'}>
+//                                 <div className="form-group">
+//                                     <label htmlFor="urlinput">Blog URL</label>
+//                                     <input readOnly={true} type="text" className="form-control" id="urlinput"
+//                                            aria-describedby="emailHelp" placeholder="Title" value={this.state.value}/>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </form>
+//
+//                     <Editor
+//                         apiKey="ihb3ayck9a9yko38nxiwbof8anmypdyekid8mpk7322xje6h"
+//                         init = {{
+//                             height:500,
+//                             menubar: false,
+//                             plugins: [
+//                                 'advlist autolink lists link image charmap print preview anchor',
+//                                 'searchreplace visualblocks code fullscreen',
+//                                 'insertdatetime media table paste code help wordcount'
+//                             ],
+//                             toolbar: 'undo redo | formatselect | bold italic backcolor | \
+//                                 alignleft aligncenter alignright alignjustify code image | \
+//                                 bullist numlist outdent indent | removeformat | fullscreen help'
+//                         }}
+//                         onEditorChange = {this.handleEditorChange}
+//                     />
+//                     <div className={'row p-2 justify-content-center'}>
+//                         <div className={'col-4 d-flex justify-content-center'}>
+//                             <button className={'btn btn-primary w-100'} onClick={this.postBlog}>Post Blog</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//
+//             </App>
+//
+//         );
+//     }
+// }
+//
 export default NewBlog;
