65f2416a2e3f375f409af1209e60a2414494d590
e-newton <ericnewton2251@gmail.com>
2020-12-09 00:52:51 -0800

    Favicon added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ const App = ({ children }) => (
   <main>
       <Head>
           <title>Eric Newton - Ramblings</title>
+          <link rel="icon" type="image/png" href="/favicon.png"/>
           <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css"/>
       </Head>
       <Header/>
