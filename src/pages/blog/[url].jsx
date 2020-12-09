ed25e9c2556e6ac5ca011287b6b2b8c0f00985c1
e-newton <ericnewton2251@gmail.com>
2020-12-09 00:33:11 -0800

    Added deleting blogs

--- src/pages/blog/[url].jsx
+++ src/pages/blog/[url].jsx
@@ -3,8 +3,9 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import App from "../../components/App";
 import Loading from "../../components/Loading";
-import {analytics} from "../../lib/firebase";
+import {analytics, firestore} from "../../lib/firebase";
 import firebase from "firebase";
+import {useAuth} from "../../hooks/useAuth";
 
 const fetcher = async (...args) => {
     const res = await fetch(...args);
@@ -15,6 +16,7 @@ function URL() {
     const router = useRouter();
     const { url } = router.query;
     const { data } = useSWR(`/api/blog/${url}`, fetcher);
+    const auth = useAuth();
 
     useEffect(() => {
         firebase.analytics().logEvent('page_view', {page_title: `${url}`})
@@ -27,10 +29,37 @@ function URL() {
         </App>)
 
     }
+
+    const edit = async() => {
+        if (auth.user){
+            await router.push(`${url}/edit`)
+        }
+    }
+
+    const deleteBlog = async() => {
+        if(auth.user && url){
+            await firestore
+                .collection('blogs')
+                .doc(url)
+                .delete()
+            await router.push(`../blog`)
+
+        }
+    }
     console.log(data)
     return (
         <App>
             <div className={'container-xl'}>
+                { auth.user &&
+                    <div className={'row justify-content-center'}>
+                        <div className={'col-2 text-center'}>
+                            <button className={'btn btn-secondary w-100'} onClick={edit}>Edit</button>
+                        </div>
+                        <div className={'col-2 text-center'}>
+                            <button className={'btn btn-danger w-100'} onClick={deleteBlog}>Delete</button>
+                        </div>
+                    </div>
+                }
                 <div className={'row justify-content-center mt-5 mb-5'}>
                     <div className={'col text-center'}>
                         <h1>{data.title}</h1>
